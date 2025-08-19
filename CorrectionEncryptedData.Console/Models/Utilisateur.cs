8bddabe9279fcffc0f6476c8a6e1898c7101853c
Thierry Morre <thierry.morre@cognitic.be>
2025-08-19 12:13:27 +0200

    Ajout utilisation du token d(identification

---
+++ CorrectionEncryptedData.Console/Models/Utilisateur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectionEncryptedData.ConsoleApp.Models
+{
+    public class Utilisateur
+    {
+        public int Id { get; }
+        public string Nom { get; }
+        public string Prenom { get; }
+        public string Email { get; }
+        public string Token { get; }
+
+        public Utilisateur(int id, string nom, string prenom, string email, string token)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+            Email = email;
+            Token = token;
+        }
+    }
+}
--- CorrectionEncryptedData.Console/Program.cs
+++ CorrectionEncryptedData.Console/Program.cs
@@ -23,21 +23,21 @@ public static class Program
         }
 
         //Enregistrement de l'utilisateur
-        if (publicKeyInfo is not null)
-        {
-            RsaEncryption rsaEncryption = new RsaEncryption(publicKeyInfo.PublicKey);
+        //if (publicKeyInfo is not null)
+        //{
+        //    RsaEncryption rsaEncryption = new RsaEncryption(publicKeyInfo.PublicKey);
 
-            using (HttpClient httpClient = new HttpClient())
-            {
-                httpClient.BaseAddress = new Uri("https://localhost:7019");
+        //    using (HttpClient httpClient = new HttpClient())
+        //    {
+        //        httpClient.BaseAddress = new Uri("https://localhost:7019");
 
-                HttpResponseMessage httpResponseMessage = httpClient.PostAsJsonAsync("api/auth/register", new { Nom = "Doe", Prenom = "John", Email = "john.doe@test.be", Passwd = rsaEncryption.Encrypt("Test1234=") }).Result;
+        //        HttpResponseMessage httpResponseMessage = httpClient.PostAsJsonAsync("api/auth/register", new { Nom = "Doe", Prenom = "John", Email = "john.doe@test.be", Passwd = rsaEncryption.Encrypt("Test1234=") }).Result;
 
-                Console.WriteLine(httpResponseMessage.IsSuccessStatusCode);
-            }
-        }
+        //        Console.WriteLine(httpResponseMessage.IsSuccessStatusCode);
+        //    }
+        //}
 
-
+        Utilisateur? utilisateur = null;
         //Login
         if (publicKeyInfo is not null)
         {
@@ -53,11 +53,35 @@ public static class Program
                 {
                     string json = httpResponseMessage.Content.ReadAsStringAsync().Result;
                     Console.WriteLine(json);
+
+                    utilisateur = JsonSerializer.Deserialize<Utilisateur>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
                 }
                 else
                 {
                     Console.WriteLine(httpResponseMessage.StatusCode);
                 }
+
+            }
+        }
+
+        if(utilisateur is not null)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {utilisateur.Token}");
+
+                httpClient.BaseAddress = new Uri("https://localhost:7019");
+
+                string[]? strings = httpClient.GetFromJsonAsync<string[]>("api/test").Result;
+
+                if(strings is not null)
+                {
+                    foreach (string s in strings)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+                
 
             }
         }
